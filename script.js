5657a8c8e880ead3d32e6262573d2f0c731a1ae6
Rahat Ibn Rafiq <rahat.rafiq@colorado.edu>
2016-06-01 00:59:20 -0600

    commit4

---
+++ script.js
@@ -0,0 +1,13 @@
+ document.getElementById("navMenu").innerHTML =
+ '<ul id="navmenu">'+
+ '<li><a href="index.html">Home</a></li>'+
+  '<li><a href="#">Batch Information</a><span class="darrow">&#9660;</span><ul class="sub1"><li><a href="batch_information/by_house.html">By House</a></li><li><a href="batch_information/by_profession.html">By Profession</a></li></ul></li>'+
+  '<li><a href="batch_fund.html">Batch Fund</a></li>'+
+  '<li><a href="families.html">Families</a></li>'+
+  '<li><a href="social.html">Social Works</a></li>'+
+  '<li><a href="pictures.html">Pictures</a></li>'+
+  '<li><a href="fun_facts.html">Fun Facts</a></li>'+
+  '<li><a href="who_we_lost.html">Who We Lost</a></li>'+
+  '<li><a href="blogs.html">Blogs</a></li>'+
+  '<li><a href="news.html">News</a></li>'+
+ '</ul>';
